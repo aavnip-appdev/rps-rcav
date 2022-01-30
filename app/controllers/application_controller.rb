8094d29af9b0f7918ce87984c9471b0b6263ffb9
aavnip <aavnip@chicagobooth.edu>
2022-01-30 19:25:32 +0000

    RPS RCAV Lecture WIP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,10 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def play_rock
+    #redirect_to("https://en.wikipedia.org/wiki/Main_Page")
+    #render({ :plain => rand(100)})
+    #render({ :html => "<h1>Hello, World! </h1>".html_safe })
+    render({ :template => "game_templates/user_rock.html.erb" })
+  end
 end
