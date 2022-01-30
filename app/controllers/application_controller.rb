4cab23e72ea945fbf6ab179714dcf1a08aec284a
aavnip <aavnip@chicagobooth.edu>
2022-01-30 19:56:05 +0000

    RPS RCAV WIP 3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,26 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
+  def homepage
+    render({ :template => "game_templates/rules.html.erb"}) 
+  end
 
   def play_rock
-    #redirect_to("https://en.wikipedia.org/wiki/Main_Page")
-    #render({ :plain => rand(100)})
-    #render({ :html => "<h1>Hello, World! </h1>".html_safe })
     render({ :template => "game_templates/user_rock.html.erb" })
   end
+
+  def play_paper
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
+  end
+    
+    render({ :template => "game_templates/user_paper.html.erb" })
+  end
+
 end
