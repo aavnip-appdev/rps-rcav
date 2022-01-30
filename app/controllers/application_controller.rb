8e8a3fdc37c4b3fc7fd95ce02137bb4da3fcc4b2
aavnip <aavnip@chicagobooth.edu>
2022-01-30 20:19:43 +0000

    RPS RCAV WIP 5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,16 @@ class ApplicationController < ActionController::Base
   end
 
   def play_rock
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+    elsif @comp_move == "scissors"
+      @outcome = "won"
+    end
+
     render({ :template => "game_templates/user_rock.html.erb" })
   end
 
@@ -20,9 +30,23 @@ class ApplicationController < ActionController::Base
       @outcome = "tied"
     elsif @comp_move == "scissors"
       @outcome = "lost"
-  end
+    end
     
     render({ :template => "game_templates/user_paper.html.erb" })
+  end
+
+  def play_scissors
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "lost"
+    elsif @comp_move == "paper"
+      @outcome = "won"
+    elsif @comp_move == "scissors"
+      @outcome = "tied"
+    end
+    
+    render({ :template => "game_templates/user_scissors.html.erb" })
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "application", :action => "homepage"})
- 
-  get("/rock", { :controller => "application", :action => "play_rock"})
+  get("/", { :controller => "application", :action => "homepage" })
 
-  get("/paper", { :controller => "application", :action => "play_paper"})
+  get("/rock", { :controller => "application", :action => "play_rock" })
 
+  get("/paper", { :controller => "application", :action => "play_paper" })
+
+  get("/scissors", { :controller => "application", :action => "play_scissors" })
 end
